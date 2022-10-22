f55bfd8826348b1cc34e6453bf16d9177215cdb1
Yegor Borisenco <pimmey@pimmey.com>
2022-10-22 13:13:05 +0800

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Yegor Borisenco on 22.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
